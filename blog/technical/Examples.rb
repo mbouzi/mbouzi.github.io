683a1eb76789394eb57049447f42bf67c93ae23c
mbouzi <mrbouzi@gmail.com>
2015-01-09 14:20:36 -0500

    added exmpls

---
+++ blog/technical/Examples.rb
@@ -0,0 +1,8 @@
+Hash = {
+	"Germany" => "Won the World Cup",
+	"Spain" => "Won the European Cup",
+	"Bayern Munich" => "Won the Champions League",
+}
+
+puts Hash["Germany"]
+puts Hash["Bayern Munich"]
