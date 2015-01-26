ac30689a77b67013183bae532b93c24c67b8eb5c
mbouzi <mrbouzi@gmail.com>
2015-01-25 21:46:44 -0500

    new example

--- blog/technical/Examples.rb
+++ blog/technical/Examples.rb
@@ -1,1 +1,25 @@
-1.up_to(10).group_by{|x| x % 2}
+class Player
+	def initialize(name,team,accuracy)
+		@name = name
+		@team = team
+		@accuracy = accuracy
+	end
+
+	def shoot
+		if @accuracy > 60
+			p "Score!!"
+		else
+			p "Miss!!"
+		end
+	end
+end
+
+
+theo = Player.new("Theo", "England", 80)
+
+flamini = Player.new("Flamini", "France", 30)
+
+
+theo.shoot
+
+flamini.shoot
