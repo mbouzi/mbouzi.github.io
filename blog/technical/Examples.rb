1273df4db58920c7c5bc2bfb03e59cd1d9e6fe03
mbouzi <mrbouzi@gmail.com>
2015-05-14 23:46:39 -0400

    new site design basics

--- blog/technical/Examples.rb
+++ blog/technical/Examples.rb
@@ -1,25 +1,48 @@
-class Player
-	def initialize(name,team,accuracy)
-		@name = name
-		@team = team
-		@accuracy = accuracy
-	end
-
-	def shoot
-		if @accuracy > 60
-			p "Score!!"
-		else
-			p "Miss!!"
-		end
+module Testing
+	def test
+		p "this is a test"
 	end
 end
 
+class Number_1
+	include Testing
+end
 
-theo = Player.new("Theo", "England", 80)
+class Number_2
+	include Testing
+end
 
-flamini = Player.new("Flamini", "France", 30)
+test_1 = Number_1.new
+test_1.test
+
+test_2 = Number_2.new
+test_2.test
 
 
-theo.shoot
 
-flamini.shoot
+
+# class Player
+# 	def initialize(name,team,accuracy)
+# 		@name = name
+# 		@team = team
+# 		@accuracy = accuracy
+# 	end
+
+# 	def shoot
+# 		if @accuracy > 60
+# 			p "Score!!"
+# 		else
+# 			p "Miss!!"
+# 		end
+# 	end
+# end
+
+
+# theo = Player.new("Theo", "England", 80)
+
+# flamini = Player.new("Flamini", "France", 30)
+
+
+# theo.shoot
+
+# flamini.shoot
