7be49ae52079f8e22a0aee27f4c14fe6b594e3eb
mbouzi <mrbouzi@gmail.com>
2016-03-07 23:19:27 -0500

    fade on intro text

---
+++ imgs/Malcolm Bouzi_files/site.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$("#header").find("li").hover(function(){
+		$(this).toggleClass("underline");
+	});
+});
+
+
+$(document).ready(function(){
+	$('.one-cap').hover(function(){
+		$('.one-desc').toggleClass('one-trans');
+	});
+	$('.two-cap').hover(function(){
+		$('.two-desc').toggleClass('two-trans');
+	});
+	$('.three-cap').hover(function(){
+		$('.three-desc').toggleClass('three-trans');
+	});
+	$('.four-cap').hover(function(){
+		$('.four-desc').toggleClass('four-trans');
+	});
+});
+
