0b7ebd780ec6446fafb5f14fab683d67db080e44
mbouzi <mrbouzi@gmail.com>
2016-03-07 23:25:31 -0500

    black on scroll enabled

--- js/site.js
+++ js/site.js
@@ -20,3 +20,23 @@ $(document).ready(function(){
 	});
 });
 
+var header = $('#welcome-section');
+var range = 1000;
+
+$(window).on('scroll', function () {
+  
+    var scrollTop = $(this).scrollTop();
+    var offset = header.offset().top;
+    var height = header.outerHeight();
+    offset = offset + height / 2;
+    var calc = 1 - (scrollTop - offset + range) / range;
+  
+    header.css({ 'opacity': calc });
+  
+    if ( calc > '1' ) {
+      header.css({ 'opacity': .35 });
+    } else if ( calc < '0' ) {
+      header.css({ 'opacity': 0 });
+    }
+  
+});
